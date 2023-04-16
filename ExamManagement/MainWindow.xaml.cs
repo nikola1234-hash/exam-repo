6b71174d3f8f5848c099d42d3bbdb70341aed5eb
Nikola Savic <nikola.savic.beograd@gmail.com>
2023-04-16 05:29:10 +0200

    Instantiated new logic for views etc

--- ExamManagement/MainWindow.xaml.cs
+++ ExamManagement/MainWindow.xaml.cs
@@ -1,114 +1,27 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace EasyTestMaker
 {
-    public partial class MainWindow: Window
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-
-        //private bool _isStudent;
-
-        //public bool IsStudent
-        //{
-        //    get { return _isStudent; }
-        //    set
-        //    {
-        //        SetField(ref _isStudent, value, nameof(IsStudent));
-        //    }
-        //}
-
-
-        //private bool _isLector;
-
-        //public bool IsLector
-        //{
-        //    get { return _isLector; }
-        //    set
-        //    {
-        //        SetField(ref _isLector, value, nameof(IsLector));
-        //    }
-        //}
-
-
-
         public MainWindow()
         {
-
-       
-
-            //InitializeComponent();
-            //if(Storage.Storage.User == "Lector" || Storage.Storage.User == "lector")
-            //{
-            //    IsLector = true;
-
-            //}
-            //else
-            //{
-            //   IsStudent = true;
-            }
+            InitializeComponent();
         }
-        /// <summary>
-        /// Create exam window open
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        //private void CreateExamButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    var createExamWindow = new CreateExamWindow();
-        //    createExamWindow.ShowDialog();
-        //}
-        ///// <summary>
-        ///// Search exam window open
-        ///// </summary>
-        ///// <param name="sender"></param>
-        ///// <param name="e"></param>
-        //private void SearchExamsButton_Click(object sender, RoutedEventArgs e)
-        //{
-        //    var searchExamsWindow = new SearchExamWindow();
-        //    searchExamsWindow.ShowDialog();
-        //}
-        ///// <summary>
-        ///// Show statistics window
-        ///// </summary>
-        ///// <param name="sender"></param>
-        ///// <param name="e"></param>
-        //private void ShowStatisctics(object sender, RoutedEventArgs e)
-        //{
-        //    var statisticsWindow = new StatisticsWindow();
-        //    statisticsWindow.Show();
-        //}
-        ///// <summary>
-        ///// Edit Exam Window
-        ///// </summary>
-        ///// <param name="sender"></param>
-        ///// <param name="e"></param>
-        //private void editExam(object sender, RoutedEventArgs e)
-        //{
-        //    UpdateExam window = new UpdateExam();
-        //    window.Show();
-        //}
-
-
-        //public event PropertyChangedEventHandler PropertyChanged;
-        //protected virtual void OnPropertyChanged(string propertyName)
-        //{
-        //    PropertyChangedEventHandler handler = PropertyChanged;
-        //    if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
-        //}
-        //protected bool SetField<T>(ref T field, T value, string propertyName)
-        //{
-        //    if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        //    field = value;
-        //    OnPropertyChanged(propertyName);
-        //    return true;
-        //}
-
-       
-        //private void searchButton_Click_1(object sender, RoutedEventArgs e)
-        //{
-        //    StudentStatsWindow window = new StudentStatsWindow();
-        //    window.Show();
-        //}
-    
+    }
 }
